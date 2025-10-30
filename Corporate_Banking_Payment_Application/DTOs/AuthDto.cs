3aeb0f648cd3ae8205afb5df9e0efaf66db678b2
Chetan Kumar <chetank9903@gmail.com>
2025-10-30 16:43:26 +0530

    feat: Added authentication

---
+++ Corporate_Banking_Payment_Application/DTOs/AuthDto.cs
@@ -0,0 +1,53 @@
+﻿using Corporate_Banking_Payment_Application.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Corporate_Banking_Payment_Application.DTOs
+{
+    public class AuthDto
+    {
+    }
+    public class RegisterDto
+    {
+        [Required, MaxLength(50)]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required, EmailAddress]
+        public string EmailId { get; set; } = string.Empty;
+
+        [Required, MinLength(6)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required, Phone]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        [Required, MaxLength(200)]
+        public string Address { get; set; } = string.Empty;
+
+        [Required]
+        public UserRole UserRole { get; set; }
+    }
+
+    public class LoginDto
+    {
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+
+    public class AuthResponseDto
+    {
+        public string Token { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+        public string EmailId { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+    }
+}
